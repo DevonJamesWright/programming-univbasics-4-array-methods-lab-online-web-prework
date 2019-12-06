0bdd213576770e4f4d661b6007ba8952c4f95e41
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-06 22:08:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+array_exp = ["booty cheeks", "coding is life", "lol"]
+array_exp.include? "booty cheeks"
 end
 
 def using_sort(array)
-
+array_exp = ["I", "butt cheeks", "coding is life", "wow"]
+array_exp.sort
 end
 
 def using_reverse(array)
-
+array_exp = ["wow", "butt cheeks", "coding is life", "arrays!"]
+array_exp.reverse
 end
 
 def using_first(array)
-
+array_exp = ["wow", "butt cheeks", "coding is life", "lol"]
+array_exp.first
 end
 
 def using_last(array)
-
+array_exp = ["booty cheeks", "butt cheeks", "coding is life", "arrays!"]
+array_exp.last
 end
 
 def using_size(array)
-
+array_exp = ["booty cheeks", "butt cheeks", "coding is life", "lol","numbies", "so smart"]
+array_exp.size
 end
